0aa6f626be87425742bf3f4d906a054bb1381fbd
Samual Daugherty <samual.daugherty@gmail.com>
2025-06-25 11:19:11 -0500

    update flourish components styling

--- src/components/EvergreenFlourish.vue
+++ src/components/EvergreenFlourish.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="evergreen-flourish -mt-[1rem] flex items-center gap-4">
-    <span class="font-alumni font-bold text-[3rem] text-black -mr-28 relative z-10">Always Relevant</span>
+    <span class="font-alumni font-bold text-[2rem] sm:text-[3rem] text-black dark:text-white -mr-48 sm:-mr-28 relative z-10">Always Relevant</span>
     <div class="w-[200px] h-[1px] bg-accent"></div>
     <span class="font-alumni font-bold text-[2rem] text-accent">Or Don't Read</span>
   </div>
--- src/components/LatestFlourish.vue
+++ src/components/LatestFlourish.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="latest-flourish -mt-[1rem] flex items-center gap-4">
-    <span class="font-alumni font-bold text-[3rem] text-black -mr-28 relative z-10">Featured</span>
+    <span class="font-alumni font-bold text-[3rem] text-black dark:text-white -mr-28 relative z-10">Featured</span>
     <div class="w-[200px] h-[1px] bg-accent"></div>
     <span class="font-alumni font-bold text-[2rem] text-accent">Read On</span>
   </div>
